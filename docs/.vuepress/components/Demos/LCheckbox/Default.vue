fd53aff8909473231dbe97c60f71c3db02c2c173
EryouHao <haoeryou@qq.com>
2018-07-27 00:17:10 +0800

    update checkbox component

--- docs/.vuepress/components/Demos/LCheckbox/Default.vue
+++ docs/.vuepress/components/Demos/LCheckbox/Default.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="">
-    <l-checkbox>选项一</l-checkbox>
+    <ul>
+      <li>
+        <l-checkbox v-model="values" :value="1">选项一</l-checkbox>
+        <l-checkbox v-model="values" :value="2">选项二</l-checkbox>
+        <div>
+          选中的数组值为：{{ values }}
+        </div>
+      </li>
+      <li>
+        <l-checkbox v-model="value2">单个选项</l-checkbox>
+        <div>单个选项值为：{{ value2 }}</div>
+      </li>
+      <li>
+        <l-checkbox v-model="value3" @change="changeHandle">监听 change 事件选项</l-checkbox>
+        <div>
+          change 传值：{{ changeValue }}
+        </div>
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      values: [],
+      value2: null,
+      value3: null,
+      changeValue: null,
+    }
+  },
+  methods: {
+    changeHandle(value) {
+      this.changeValue = value
+      console.log(value)
+    },
   },
 }
 </script>
--- src/components/LCheckbox/LCheckbox.vue
+++ src/components/LCheckbox/LCheckbox.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="live-checkbox-container">
-    <div class="live-checkbox-icon">
-      <input type="checkbox">
+  <div class="live-checkbox-container" @click="handleChange">
+    <div class="live-checkbox-icon" :class="{ 'is-check': isChecked }">
+      <input type="checkbox" class="live-checkbox-input">
+      <i class="material-icons live-icon">check</i>
     </div>
     <label class="live-checkbox-label">
       <slot></slot>
@@ -16,13 +17,51 @@ export default {
     prop: 'checked',
   },
   props: {
-    value: {},
+    value: {
+      type: [Array, String, Number, Object, Boolean],
+      default: true,
+    },
     checked: {},
     disabled: Boolean,
     readonly: Boolean,
   },
   data() {
     return {}
+  },
+  computed: {
+    isChecked() {
+      const { checked, value } = this
+      return Array.isArray(checked) ? checked.includes(value) : checked === value
+    },
+  },
+  mounted() {
+    console.log(this.checked, this.value, this.checked)
+  },
+  methods: {
+    handleChange() {
+      if (!this.disabled && !this.readonly) {
+        if (Array.isArray(this.checked)) {
+          this.handleArray()
+        } else {
+          this.handleValue()
+        }
+      }
+    },
+    handleArray() {
+      const { checked } = this
+      if (checked.includes(this.value)) {
+        checked.splice(checked.indexOf(this.value), 1)
+      } else {
+        checked.push(this.value)
+      }
+      this.$emit('input', checked)
+      this.$emit('change', checked)
+    },
+    handleValue() {
+      const newValue = this.checked === this.value ? null : this.value
+      this.$emit('input', newValue)
+      this.$emit('change', newValue)
+    }
   },
 }
 </script>
