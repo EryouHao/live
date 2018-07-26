285a6a90370e27e07476648f663558fc3f8382fe
EryouHao <eryouhao@gmail.com>
2018-07-26 09:29:50 +0800

    feat: add change event to radio component & update radio component ui

--- docs/.vuepress/components/Demos/LRadio/Default.vue
+++ docs/.vuepress/components/Demos/LRadio/Default.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="">
-    <l-radio v-model="radioValue" l-value="1">选项一</l-radio>
-    <l-radio v-model="radioValue" l-value="2">选项二</l-radio>
-    值：{{ radioValue }}
+    <ul>
+      <li><l-radio v-model="radioValue" value="1">选项一</l-radio></li>
+      <li><l-radio v-model="radioValue" value="2">选项二</l-radio></li>
+      <li><l-radio v-model="radioValue" value="3" disabled>禁用选项</l-radio></li>
+      <li><l-radio v-model="radioValue" value="4" readonly>只读选项</l-radio></li>
+      <li><l-radio v-model="radioValue" value="5" @change="handleChange">监听 change 事件</l-radio></li>
+    </ul>
+    <ul>
+      <li>值：{{ radioValue }}</li>
+      <li>change 传值：{{ changeValue }}</li>
+    </ul>
   </div>
 </template>
 
@@ -10,8 +18,15 @@
 export default {
   data() {
     return {
-      radioValue: null,
+      radioValue: '1',
+      changeValue: null,
     }
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value)
+      this.changeValue = value
+    },
   },
 }
 </script>
--- src/components/LRadio/LRadio.vue
+++ src/components/LRadio/LRadio.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="live-radio-container" @click="handleChange">
+  <div class="live-radio-container" @click="handleChange" :class="{ 'is-disabled': disabled }">
     <div class="live-radio-icon" :class="{ 'is-check': isChecked }">
       <input
         class="live-radio-input"
         type="radio"
-        v-bind="$attrs"
         :checked="isChecked"
-        v-on="listeners"
+        :disabled="disabled"
+        :readonly="readonly"
+        @input="handleChange"
       >
     </div>
     <label class="live-radio-label">
@@ -18,27 +19,28 @@
 <script>
 export default {
   name: 'LRadio',
+  model: {
+    prop: 'checked',
+  },
   props: {
     value: {},
-    lValue: {},
-  },
-  data() {
-    return {
-    }
+    checked: {},
+    disabled: Boolean,
+    readonly: Boolean,
   },
   computed: {
-    listeners() {
-      return {
-        input: event => this.$emit('input', this.lValue),
-      }
-    },
     isChecked() {
-      return this.lValue === this.value
+      return this.checked === this.value
     },
   },
   methods: {
     handleChange() {
-      this.$emit('input', this.lValue)
+      if (!this.disabled && !this.readonly) {
+        this.$emit('input', this.value)
+        if (this.checked !== this.value) {
+          this.$emit('change', this.value)
+        }
+      }
     },
   },
 }
