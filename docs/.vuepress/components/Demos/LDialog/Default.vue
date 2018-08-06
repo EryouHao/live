a3179da6f8a52c590ff668ac06b86d86d69c5f0e
EryouHao <eryouhao@gmail.com>
2018-08-06 16:07:31 +0800

    refactor: update dialog component

--- docs/.vuepress/components/Demos/LDialog/Default.vue
+++ docs/.vuepress/components/Demos/LDialog/Default.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="">
-    <l-dialog></l-dialog>
+    <l-button type="primary" @click="visible = true">open</l-button>
+    <l-dialog v-model="visible" title="提示">
+      这是一条提示信息
+    </l-dialog>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      visible: false,
+    }
   },
 }
 </script>
--- src/components/LDialog/LDialog.vue
+++ src/components/LDialog/LDialog.vue
@@ -1,14 +1,52 @@
 <template>
-  <div class="">
-    hello
-  </div>
+  <transition name="fade-select">
+    <div class="live-dialog-container" v-show="value">
+      <div class="live-dialog" v-click-outside="close">
+        <div class="live-dialog-header">
+          <div class="dialog-title">{{ title }}</div>
+          <div class="dialog-close" @click="close">
+            <i class="material-icons">close</i>
+          </div>
+        </div>
+        <div class="live-dialog-content">
+          <slot>
+
+          </slot>
+        </div>
+        <div class="live-dialog-footer">
+          <l-button type="primary" @click="close">确定</l-button>
+        </div>
+        <slot name="footer">
+
+        </slot>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'LDialog',
+  props: {
+    value: {},
+    title: {
+      type: String,
+      default: '',
+    },
+    width: [Number, String],
+  },
   data() {
     return {}
+  },
+  mounted() {
+
+  },
+  methods: {
+    close() {
+      if (this.value) {
+        this.$emit('input', false)
+      }
+    },
   },
 }
 </script>
